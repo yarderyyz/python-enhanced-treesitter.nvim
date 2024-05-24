12b5b6181556c1da30d26d31631f6f0e0bcb6577
Lee Gauthier <lee.d.gauthier@gmail.com>
2024-05-24 18:11:39 +0200

    Try whole string

--- queries/python/injections.scm
+++ queries/python/injections.scm
@@ -2,6 +2,5 @@
   function: (attribute
     attribute: (identifier) @attribute (#eq? @attribute "query"))
   (argument_list
-    (string
-      (string_content) @injection.content
-      (#set! injection.language "sql"))))
+    (string) @injection.content
+      (#set! injection.language "sql")))
